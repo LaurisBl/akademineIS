046df4c274290d3334801177e0fc2a02fa23aa03
LaurisBl <laurysss245@gmail.com>
2023-11-21 13:26:53 +0200

    Add project files.

---
+++ akademineIS/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace akademineIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            loginButton = new Button();
+            SuspendLayout();
+            // 
+            // loginButton
+            // 
+            loginButton.Anchor = AnchorStyles.Bottom;
+            loginButton.Location = new Point(347, 345);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(113, 60);
+            loginButton.TabIndex = 0;
+            loginButton.Text = "Prisijungti";
+            loginButton.UseVisualStyleBackColor = true;
+            loginButton.Click += loginButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(800, 450);
+            Controls.Add(loginButton);
+            Name = "Form1";
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button loginButton;
+    }
+}
